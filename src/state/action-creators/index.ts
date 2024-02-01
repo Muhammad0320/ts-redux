266fc1548559b9f50bc8ee456f69864f24013bb7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-02-01 08:29:22 +0100

    chore: handle index.ts

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,3 +1,18 @@
+import { ActionsTypes } from "../actioonTypes";
+
 const searchRepository = (term: string) => {
-  return async (dispatch: any) => {};
+  return async (dispatch: any) => {
+    dispatch({
+      type: ActionsTypes.SEARCH_REPO,
+    });
+
+    try {
+    } catch (error) {
+      if (error instanceof Error)
+        dispatch({
+          type: ActionsTypes.SEARCH_REPO_ERROR,
+          payload: error.message,
+        });
+    }
+  };
 };
